1b395f91c85d2079c68166d091e9538f470fef56
masterXEN1 <124404225+masterXEN1@users.noreply.github.com>
2023-02-03 23:14:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import random
 import time
 from tabulate import tabulate # imports our table
-
-
+VocabArray = ["help", "savedData", "EnterVocab"] # Creates Array
 
 randomNumber = random.randint(1, 2)
 
 def addVocabulary():
     vocab = input("Enter Vocabulary: \n")
     print("Vocabulary/Variable Saved. |", vocab)
-    VocabArray = [] # Creates Array
     VocabArray.append(vocab) # Saves Inputted Variable into a array which you can access.
+
+
 
 if randomNumber == 1: # Randomising Greetings
     print("Hello there!")
@@ -32,12 +32,32 @@ Commands = [ # Commands that we have
 
 head = ["Commands", "Description"] # header of the help menu
 
+
+
 print(tabulate(Commands, headers=head, tablefmt="grid")) # Prints the help menu
 
 Decision = input("Would you like to submit some vocabulary? (Y), (N) \n")
 
+
+def EnterCommand():
+    command = input("Please enter a command.")
+
+    if command == "help":
+        print(tabulate(Commands, headers=head, tablefmt="grid")) # Prints the help menu
+        EnterCommand()
+    elif command == "savedData":
+        print(VocabArray)
+        EnterCommand()
+    elif command == "EnterVocab":
+        addVocabulary()
+        EnterCommand()
+    elif command in VocabArray:
+        print(command)
+        EnterCommand()
+
 if Decision == "N":
     print("Okay!")
+    EnterCommand()
 elif Decision == "Y":
     addVocabulary() # adds the vocabulary to Line 13
-
+    EnterCommand()
